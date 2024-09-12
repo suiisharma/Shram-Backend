b2fdf8ea32cbe7cc6c43983a413568d016187b5e
sourav sharma <127027218+suiisharma@users.noreply.github.com>
2024-09-13 05:02:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,11 +16,14 @@ connectDB();
 
 // Middleware
 
-app.use( cors({
-    origin: ["https://shram-frontend.vercel.app"],
+
+app.use(
+  cors({
+    origin: [process.env.frontend_url],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
-  }));
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
